9c0a8700ba768dcd03dc657b196857c33d959ddc
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-10-11 15:15:40 +0200

    Fix categories

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,14 @@
 -- CATEGORY
-INSERT INTO CATEGORY (id, version, name) VALUES (1000, 1, 'Romance');
-INSERT INTO CATEGORY (id, version, name) VALUES (1001, 1, 'Arts & Photography');
-INSERT INTO CATEGORY (id, version, name) VALUES (1002, 1, 'Biographies & Memoirs');
-INSERT INTO CATEGORY (id, version, name) VALUES (1003, 1, 'Business & Money');
-INSERT INTO CATEGORY (id, version, name) VALUES (1004, 1, 'Comics & Graphic Novels');
-INSERT INTO CATEGORY (id, version, name) VALUES (1005, 1, 'Computers & Technology');
-INSERT INTO CATEGORY (id, version, name) VALUES (1006, 1, 'Cookbooks, Food & Wine');
-INSERT INTO CATEGORY (id, version, name) VALUES (1007, 1, 'Engineering & Transportation');
-INSERT INTO CATEGORY (id, version, name) VALUES (1008, 1, 'Health, Fitness & Dieting');
-INSERT INTO CATEGORY (id, version, name) VALUES (1009, 1, 'Literature & Fiction');
-INSERT INTO CATEGORY (id, version, name) VALUES (1010, 1, 'Mystery, Thriller & Suspense');
-INSERT INTO CATEGORY (id, version, name) VALUES (1011, 1, 'Science & Math');
-INSERT INTO CATEGORY (id, version, name) VALUES (1012, 1, 'Science Fiction & Fantasy');
+INSERT INTO CATEGORY (id, name) VALUES (1000, 'Romance');
+INSERT INTO CATEGORY (id, name) VALUES (1001, 'Arts & Photography');
+INSERT INTO CATEGORY (id, name) VALUES (1002, 'Biographies & Memoirs');
+INSERT INTO CATEGORY (id, name) VALUES (1003, 'Business & Money');
+INSERT INTO CATEGORY (id, name) VALUES (1004, 'Comics & Graphic Novels');
+INSERT INTO CATEGORY (id, name) VALUES (1005, 'Computers & Technology');
+INSERT INTO CATEGORY (id, name) VALUES (1006, 'Cookbooks, Food & Wine');
+INSERT INTO CATEGORY (id, name) VALUES (1007, 'Engineering & Transportation');
+INSERT INTO CATEGORY (id, name) VALUES (1008, 'Health, Fitness & Dieting');
+INSERT INTO CATEGORY (id, name) VALUES (1009, 'Literature & Fiction');
+INSERT INTO CATEGORY (id, name) VALUES (1010, 'Mystery, Thriller & Suspense');
+INSERT INTO CATEGORY (id, name) VALUES (1011, 'Science & Math');
+INSERT INTO CATEGORY (id, name) VALUES (1012, 'Science Fiction & Fantasy');
