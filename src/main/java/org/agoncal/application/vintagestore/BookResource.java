40b62d0d6a4d9fbf2e5516fa80f106d60fb06a03
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-10-11 15:10:14 +0200

    Init

---
+++ src/main/java/org/agoncal/application/vintagestore/BookResource.java
@@ -0,0 +1,19 @@
+package org.agoncal.application.vintagestore;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/books")
+public class BookResource {
+
+    @GET
+    @Path("/categories")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Category> getAllCategories() {
+        return Category.listAll();
+    }
+}
