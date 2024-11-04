b672e1c98f2f05f9f48cc460223dda1e6ea4eb95
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-11-04 15:59:56 +0100

    Tools

--- src/main/java/org/agoncal/application/vintagestore/chat/ItemsInStockTools.java
+++ src/main/java/org/agoncal/application/vintagestore/chat/ItemsInStockTools.java
@@ -1,11 +1,18 @@
 package org.agoncal.application.vintagestore.chat;
 
 import dev.langchain4j.agent.tool.Tool;
+import dev.langchain4j.service.V;
+import org.agoncal.application.vintagestore.model.Item;
+import org.jboss.logging.Logger;
 
 public class ItemsInStockTools {
 
-  @Tool("Returns true if the item (CD or Book) in stock")
-  boolean isItemInStock() {
-    return true;
+  private static final Logger LOG = Logger.getLogger(VintageStoreChatBot.class);
+
+  @Tool("Given a title, returns true if the item (CD or Book) is available in stock")
+  boolean isItemInStock(@V("CD or Book title") String title) {
+    long nbOfAvailableItems = Item.count("lower(title) like lower(concat('%', ?1,'%'))", title);
+    LOG.info("Number of available items for " + title + " is " + nbOfAvailableItems);
+    return nbOfAvailableItems > 0;
   }
 }
