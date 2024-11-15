eef29084bec4932345255c2563357d4d6f7e7aa0
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-11-15 06:53:15 +0100

    Test

---
+++ src/test/java/org/agoncal/application/vintagestore/chat/VintageStoreChatAssistantTest.java
@@ -0,0 +1,13 @@
+package org.agoncal.application.vintagestore.chat;
+
+import org.junit.jupiter.api.Test;
+
+class VintageStoreChatAssistantTest {
+
+  @Test
+  void shouldChat() {
+    System.out.println("###########");
+    System.out.println("###########");
+    System.out.println("###########");
+  }
+}
