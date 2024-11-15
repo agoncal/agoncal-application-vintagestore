d36ba675e09e825c292fc17978f71b65f852d849
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-11-15 09:20:56 +0100

    Test

--- src/main/java/org/agoncal/application/vintagestore/chat/VintageStoreChatAssistant.java
+++ src/main/java/org/agoncal/application/vintagestore/chat/VintageStoreChatAssistant.java
@@ -9,6 +9,7 @@ public interface VintageStoreChatAssistant {
     You know all the legal terms and conditions of the company, as well as all the available items currently in stock.
     Keep your answers short and to the point.
     If the question is related to "Vintage Store" but you don't know the answer, say 'I don't know' and explain why.
+    If the question is NOT related to "Vintage Store", answer the question, but warn that you are a chat bot that primarily answers "Vintage Store" related questions.
     """)
   String chat(String userMessage);
 }
--- src/test/java/org/agoncal/application/vintagestore/chat/VintageStoreChatAssistantTest.java
+++ src/test/java/org/agoncal/application/vintagestore/chat/VintageStoreChatAssistantTest.java
@@ -10,6 +10,20 @@ class VintageStoreChatAssistantTest {
   @Test
   void shouldChat() throws Exception {
     VintageStoreChatAssistant assistant = assistant(embeddingStore(), model());
+
     System.out.println(assistant.chat("Hello, my name is Antonio?"));
+    // Hello Antonio! How can I assist you today with information about "Vintage Store"?
+
+    System.out.println(assistant.chat("Who are you?"));
+    // I am the chat bot for "Vintage Store." I'm here to assist you with information about our products, services, and legal terms. If you have any questions or need help, feel free to ask!
+
+    System.out.println(assistant.chat("Where is the company located?"));
+    // The Vintage Store is located in Paris, France. If you have any more questions, feel free to ask!
+
+    System.out.println(assistant.chat("What is the capital of France?"));
+    // The capital of France is Paris. Please note that I am a chat bot primarily designed to answer questions related to "Vintage Store." If you have any questions about the store, feel free to ask!
+
+    System.out.println(assistant.chat("What's my name?"));
+    // You mentioned earlier that your name is Antonio. If you have any other questions or need assistance, feel free to ask!
   }
 }
