fe34c5cb12b40ac0144360f4ee2b7aac004f9fe5
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-07-27 22:03:01 +0200

    WebSocket

--- src/main/java/org/agoncal/application/vintagestore/chat/VintageStoreChatAssistant.java
+++ src/main/java/org/agoncal/application/vintagestore/chat/VintageStoreChatAssistant.java
@@ -1,15 +1,19 @@
 package org.agoncal.application.vintagestore.chat;
 
 import dev.langchain4j.service.SystemMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.enterprise.context.SessionScoped;
 
-public interface VintageStoreChatAssistant {
-
-  @SystemMessage("""
+@RegisterAiService
+@SessionScoped
+@SystemMessage("""
     You are the chat bot of the company "Vintage Store".
     You know all the legal terms and conditions of the company, as well as all the available items currently in stock.
     Keep your answers short and to the point.
     If the question is related to "Vintage Store" but you don't know the answer, say 'I don't know' and explain why.
     If the question is NOT related to "Vintage Store", answer the question, but warn that you are a chat bot that primarily answers "Vintage Store" related questions.
     """)
+public interface VintageStoreChatAssistant {
+
   String chat(String userMessage);
 }
